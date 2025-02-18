9ed0f5a47c0f3787b06135b65432b32fe137615e
AlvaroMartins92 <alvarompg@hotmail.com>
2025-02-17 21:31:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function gerarLink() {
     let links = {
         "oleosa-acne": "https://www.mercadolivre.com.br/produto-pele-oleosa-acne",
         "oleosa-rugas": "https://www.mercadolivre.com.br/produto-pele-oleosa-rugas",
-        "seca-manchas": "https://www.mercadolivre.com.br/produto-pele-seca-manchas",
+        "seca-manchas": "https://www.mercadolivre.com.br/p/MLB41738809",
         "mista-acne": "https://www.mercadolivre.com.br/produto-pele-mista-acne",
         "sensivel-rugas": "https://www.mercadolivre.com.br/produto-pele-sensivel-rugas"
     };
